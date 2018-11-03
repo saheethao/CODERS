02742f31807b874dba635c9a75dcebaf187ab0e1
saheethao <thao.sahee@uwlax.edu>
2018-11-02 23:45:35 -0500

    Create Operators.java

---
+++ session0/02-operators/Operators.java
@@ -0,0 +1,19 @@
+public class Operators {
+	public static void main(String[] args) {
+		int x;
+		x = 9;
+		int y;
+		y = 3;
+		int answer;
+		answer = x + y;
+		System.out.println(answer);
+		
+		int a = 2;
+		int b = 4;
+		int c = 8;
+		int answer1 = (a + b) * c;
+		int answer2 = a + b * c;
+		System.out.println(answer1);
+		System.out.println(answer2);
+	}
+}

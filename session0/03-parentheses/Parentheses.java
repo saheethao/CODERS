c203548867b07a71ea6cd91438bf426dcea9051c
saheethao <thao.sahee@uwlax.edu>
2018-11-02 23:55:57 -0500

    Create Parentheses.java

---
+++ session0/03-parentheses/Parentheses.java
@@ -0,0 +1,14 @@
+
+public class Parentheses {
+
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 4;
+		int c = 8;
+		int answer1 = (a + b) * c;
+		int answer2 = a + b * c;
+		System.out.println(answer1);
+		System.out.println(answer2);
+	}
+
+}

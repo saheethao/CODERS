6b4609ab88156d24142e7bf7a5afe8bfb9a1b8a4
saheethao <thao.sahee@uwlax.edu>
2018-11-02 23:54:37 -0500

    Update Operators.java

--- session0/02-operators/Operators.java
+++ session0/02-operators/Operators.java
@@ -7,13 +7,5 @@ public class Operators {
 		int answer;
 		answer = x + y;
 		System.out.println(answer);
-		
-		int a = 2;
-		int b = 4;
-		int c = 8;
-		int answer1 = (a + b) * c;
-		int answer2 = a + b * c;
-		System.out.println(answer1);
-		System.out.println(answer2);
 	}
 }

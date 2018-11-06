41a79cff78927cd5f87046f5a97a89b4111345d1
saheethao <thao.sahee@uwlax.edu>
2018-11-06 15:08:23 -0600

    Update Calculator.java

--- session0/07-calculator/Calculator.java
+++ session0/07-calculator/Calculator.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
 
+public class Calculator {
+	
+	public static void main(String[] args) {
+		//Declare variables
+		int firstInt;
+		String operator;
+		int secondInt;
+		
+		//Get user inputs
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter an integer: ");
+		firstInt = scan.nextInt();
+		System.out.print("Enter an operator: ");
+		operator = scan.next();
+		System.out.print("Enter an integer: ");
+		secondInt = scan.nextInt();
+		
+		if (operator.equals("+")) {
+			System.out.println(firstInt + secondInt);
+		} else {
+			System.out.println("Invalid operator!");
+		}
+	}
+
+}

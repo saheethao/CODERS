2eca10fd390e8a88492e90b2168f0c1ef6b9545e
saheethao <thao.sahee@uwlax.edu>
2018-12-01 01:23:38 -0600

    Update CarSolution.java

--- session1/05-car-race-solution/CarSolution.java
+++ session1/05-car-race-solution/CarSolution.java
@@ -1,33 +1,35 @@
 
-public class Car {
+public class CarSolution {
 	
+	/* Car Attributes */
 	private String name;
 	private int position;
 	private int velocity;
 	private int acceleration;
-	//private int gas;
+	private int gas;
 	
-	public Car(String name, int startingPosition, int acceleration) {
+	/* Car Constructor */
+	public CarSolution(String name, int startingPosition, int acceleration) {
 		this.name = name;
 		this.position = startingPosition;
 		this.velocity = 0;
 		this.acceleration = acceleration;
-		//this.gas = 20;
+		this.gas = 20;
 	}
 	
 	/**
 	 * This method is the move() method you have seen before!
 	 */
 	public void move() {
-		/* Insert code here! */
-		
-		/* Change the code so that the gas reduces based on the acceleration. 
-		 * A car uses 2 units of gas for every 1 meter per second per second (acceleration).
-		 * If a car has 0 or less gas, it should slow down (have negative acceleration).
-		 */
+		if (gas <= 0) {
+			acceleration = acceleration - 5;
+			System.out.print("Out of gas! ");
+		} else {
+			gas = gas - 2 * acceleration;
+		}
 		velocity = velocity + acceleration;
 		position = position + velocity;
-		System.out.println("Vroom! " + name + " is at position " + position + " and travelling at " + velocity + " m/s!");
+		System.out.println("Vroom! " + name + " is at position " + position + " and travelling at " + velocity + " m/s! ");
 	}
 	
 	/**
